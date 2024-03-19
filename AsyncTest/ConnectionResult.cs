0c70926385829305e38eb3da542f7bed22f3d0c7
iHouqLF <73959649+JoelHer@users.noreply.github.com>
2024-03-19 17:44:02 +0100

    File Refactor for ConnectionResult

---
+++ AsyncTest/ConnectionResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static KeySwapper.ServerInfoGatherer;
+
+namespace KeySwapper
+{
+    public class ConnectionResult
+    {
+        public int id;
+        public string result;
+        public string newPubKey;
+        public string newPrivKey;
+        public string passphrase;
+        public EndServer host;
+        public ServerInfo gatheredInfo;
+
+        public ConnectionResult(EndServer host, int id, string result, ServerInfo gatheredInfo, string newPubKey = "", string newPrivKey = "", string passphrase = "")
+        {
+            this.host = host;
+            this.id = id;
+            this.result = result;
+            this.newPubKey = newPubKey;
+            this.newPrivKey = newPrivKey;
+            this.passphrase = passphrase;
+            this.gatheredInfo = gatheredInfo;
+        }
+        public int GetId() { return id; }
+        public EndServer GetHost() { return host; }
+        public string GetResult() { return result; }
+        public string GetNewPubKey() { return newPubKey; }
+        public string GetNewPrivKey() { return newPrivKey; }
+        public string GetPassphrase() { return passphrase; }
+        public ServerInfo GetGatheredData() { return gatheredInfo; }
+    }
+}
--- AsyncTest/Program.cs
+++ AsyncTest/Program.cs
@@ -179,35 +179,6 @@ namespace AsyncTest
             return new ConnectionResult(_server, _workerID, result.GetResult(), result.GetGatheredData(), result.GetNewPubKey(), result.GetNewPrivKey(), result.GetPassphrase());
         }
 
-        public class ConnectionResult
-        {
-            public int id;
-            public string result;
-            public string newPubKey;
-            public string newPrivKey;
-            public string passphrase;
-            public EndServer host;
-            public ServerInfo gatheredInfo;
-
-            public ConnectionResult(EndServer host, int id, string result, ServerInfo gatheredInfo, string newPubKey = "", string newPrivKey = "", string passphrase = "")
-            {
-                this.host = host;
-                this.id = id;
-                this.result = result;
-                this.newPubKey = newPubKey;
-                this.newPrivKey = newPrivKey;
-                this.passphrase = passphrase;
-                this.gatheredInfo = gatheredInfo;
-            }
-            public int GetId() { return id; }
-            public EndServer GetHost() { return host; }
-            public string GetResult() { return result; }
-            public string GetNewPubKey() {  return newPubKey; }
-            public string GetNewPrivKey() {  return newPrivKey; }
-            public string GetPassphrase() {  return passphrase; }
-            public ServerInfo GetGatheredData() {  return gatheredInfo; }
-        }
-
         public async Task<ConnectionResult> SwapKeyAsync(EndServer _endServer, string _host, string _username)
         {
             if (_username == "")
